80d0cbfd20a453c99bfa4ebb1131365d89b3dd1b
Entkenntnis <Entkenntnis@users.noreply.github.com>
2026-05-09 06:20:13 +0200

    a bit of clean up

--- problems/tptp/PUZ001.lean
+++ problems/tptp/PUZ001.lean
@@ -1,8 +1,8 @@
 -- grind failed to find a pattern for
 --    h_noone_hates_everyone : ∀ x, ∃ y, ¬ hates x y
--- we pre-specialize the hypothesis to each of the suspects, this should not change the difficulty 
+-- we pre-specialize the hypothesis to each of the suspects, this should not change the difficulty
 
-def dreadbury_mansion : (D : Type) -> (agatha : D) -> (butler : D) -> (charles : D) -> 
+def dreadbury_mansion : (D : Type) -> (agatha : D) -> (butler : D) -> (charles : D) ->
     (lives : D -> Prop) -> (killed : D -> D -> Prop) -> (hates : D -> D -> Prop) -> (richer : D -> D -> Prop) ->
     (h_exists : @Exists D (fun (x : D) => And (lives x) (killed x agatha))) ->
     (h_only_residents : forall (x : D), lives x -> Or (@Eq D x agatha) (Or (@Eq D x butler) (@Eq D x charles))) ->
@@ -13,6 +13,9 @@ def dreadbury_mansion : (D : Type) -> (agatha : D) -> (butler : D) -> (charles :
     (h_butler_hates_non_richer_than_agatha : forall (x : D), Not (richer x agatha) -> hates butler x) ->
     (h_butler_hates_agatha_hates : forall (x : D), hates agatha x -> hates butler x) ->
     (h_noone_hates_everyone : forall (x : D), @Exists D (fun (y : D) => Not (hates x y))) ->
+    -- these three additional theorems help to fix an issue with grind
+    -- that prevents it from finding local E-match pattern because of
+    -- the existential within forall
     (h_noone_hates_everyone_agatha : @Exists D (fun (y : D) => Not (hates agatha y))) ->
     (h_noone_hates_everyone_butler : @Exists D (fun (y : D) => Not (hates butler y))) ->
     (h_noone_hates_everyone_charles : @Exists D (fun (y : D) => Not (hates charles y))) ->
