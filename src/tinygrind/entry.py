214944a5ce5f8bf5b526778f284b85e816e7c73c
Entkenntnis <Entkenntnis@users.noreply.github.com>
2026-06-05 16:14:35 +0200

    getting a feeling for it

--- src/tinygrind/entry.py
+++ src/tinygrind/entry.py
@@ -2,7 +2,9 @@
 # and will generate a proof for this exact definition
 
 from scaffolding.printer import print_term
-from scaffolding.syntax import Definition, ElabTactic, Pi, Term
+from scaffolding.syntax import Definition, ElabTactic, Pi, Sort, Term
+
+import sys
 
 
 def tinygrind(definition: Definition) -> Term:
@@ -12,8 +14,26 @@ def tinygrind(definition: Definition) -> Term:
 
     context: list[tuple[str | None, Term]] = []
 
+    theType: str | None = None
+
     while isinstance(theorem, Pi):
-        context.append((theorem.var, theorem.var_type))
+        var = theorem.var
+        var_type = theorem.var_type
+
+        if (isinstance(var_type, Sort)) and var_type.level == 1:
+            if theType == None:
+                print(f"registering the variable type as {var}")
+                theType = var
+            else:
+                print("[!] We are not supporting multiple types yet")
+                return ElabTactic("sorry")
+
+        # it's a little bit tricky, because the interface will work through
+        # all possible system states
+
+        print(f"var: {var}, var_type: {print_term(var_type)}")
+
+        context.append((var, var_type))
         theorem = theorem.body
 
     goal = context.pop()
@@ -21,4 +41,5 @@ def tinygrind(definition: Definition) -> Term:
     print(f"    context: {", ".join([f"{v} : {print_term(t)}" for (v, t) in context])}")
     print(f"    goal: {print_term(goal[1])}")
 
+    sys.exit()
     return ElabTactic("sorry")
