ecc26a18dff9ca07ee97989da4f7dbf62ebb929d
Entkenntnis <Entkenntnis@users.noreply.github.com>
2026-06-23 11:17:44 +0200

    work in progress right now

--- src/tinygrind/entry.py
+++ src/tinygrind/entry.py
@@ -3,11 +3,19 @@
 
 from typing import Literal
 from scaffolding.printer import print_term
-from scaffolding.syntax import Definition, ElabTactic, Pi, Sort, Term, Var
+from scaffolding.syntax import App, Definition, ElabTactic, Pi, Sort, Term, Var
 
 import sys
 
-from tinygrind.egraph import Constant, EGraph, FunctionSymbol, PredicateSymbol
+from tinygrind import egraph
+from tinygrind.egraph import (
+    Constant,
+    EGraph,
+    Equals,
+    FunctionSymbol,
+    PredicateApplication,
+    PredicateSymbol,
+)
 
 type Env = dict[str, Literal["type", "constant", "function", "predicate"]]
 
@@ -30,46 +38,6 @@ def tinygrind(definition: Definition) -> Term:
 
     egraph = EGraph()
 
-    # HELPERS
-    def isFunction(type: Term, env: Env) -> bool:
-        # check if the type is a function
-        if (
-            isinstance(type, Pi)
-            and isinstance(type.var_type, Var)
-            and env[type.var_type.name] == "type"
-        ):
-            # we have a function application
-            if isinstance(type.body, Var) and env[type.body.name] == "type":
-                return True
-            else:
-                return isFunction(type.body, env)
-
-        return False
-
-    def isPredicate(type: Term, env: Env) -> bool:
-        # check if the type is a function
-        if (
-            isinstance(type, Pi)
-            and isinstance(type.var_type, Var)
-            and env[type.var_type.name] == "type"
-        ):
-            # we have a function application
-            if isinstance(type.body, Sort) and type.body.level == 0:
-                return True
-            else:
-                return isPredicate(type.body, env)
-
-        return False
-
-    def compute_arity(t: Term) -> int:
-        count = 0
-        while isinstance(t, Pi):
-            count += 1
-            t = t.body
-        return count
-
-    # ------
-
     for name, type in context:
         if isinstance(type, Sort) and type.level == 1 and name:
             if "type" in env.values():
@@ -89,8 +57,64 @@ def tinygrind(definition: Definition) -> Term:
         elif name and isPredicate(type, env):
             arity = compute_arity(type)
             arities[name] = arity
-            env[name] = "function"
+            env[name] = "predicate"
             print(f"    - Adding new predicate {name} with arity {arity}")
             _ = egraph.addTerm(PredicateSymbol(name, arity))
+        elif isinstance(type, App):
+            print(f"    - (todo) add hypothesis {print_term(type)} to egraph")
+            eg_term = lean_to_egraph(type, env, arities)
+            if not isinstance(eg_term, Equals) or not isinstance(
+                eg_term, PredicateApplication
+            ):
+                pass
+                # raise RuntimeError("Egraph Term must be a predicate")
+            # _ = egraph.addTerm(eg_term)
         else:
-            print(f"TODO: Handling {name} with type {print_term(type)}")
+            print(f"TODO: Handling {name} / {type} with type {print_term(type)}")
+
+    # TODO: negate goal, check for bottom, return sorry or proof (todo)
+
+
+def isFunction(type: Term, env: Env) -> bool:
+    # check if the type is a function
+    if (
+        isinstance(type, Pi)
+        and isinstance(type.var_type, Var)
+        and env[type.var_type.name] == "type"
+    ):
+        # we have a function application
+        if isinstance(type.body, Var) and env[type.body.name] == "type":
+            return True
+        else:
+            return isFunction(type.body, env)
+
+    return False
+
+
+def isPredicate(type: Term, env: Env) -> bool:
+    # check if the type is a function
+    if (
+        isinstance(type, Pi)
+        and isinstance(type.var_type, Var)
+        and env[type.var_type.name] == "type"
+    ):
+        # we have a function application
+        if isinstance(type.body, Sort) and type.body.level == 0:
+            return True
+        else:
+            return isPredicate(type.body, env)
+
+    return False
+
+
+def compute_arity(t: Term) -> int:
+    count = 0
+    while isinstance(t, Pi):
+        count += 1
+        t = t.body
+    return count
+
+
+def lean_to_egraph(term: Term, env: Env, arities: dict[[str, str]]) -> egraph.Term:
+    # NEXTUP
+    pass
