45500ffa92750a4944653abfa67ac33d1c5499b4
Entkenntnis <Entkenntnis@users.noreply.github.com>
2026-06-23 10:39:34 +0200

    working on integration

--- src/tinygrind/entry.py
+++ src/tinygrind/entry.py
@@ -7,6 +7,10 @@ from scaffolding.syntax import Definition, ElabTactic, Pi, Sort, Term, Var
 
 import sys
 
+from tinygrind.egraph import Constant, EGraph, FunctionSymbol, PredicateSymbol
+
+type Env = dict[str, Literal["type", "constant", "function", "predicate"]]
+
 
 def tinygrind(definition: Definition) -> Term:
     print("  > tinygrind")
@@ -16,34 +20,77 @@ def tinygrind(definition: Definition) -> Term:
     context: list[tuple[str | None, Term]] = []
 
     while isinstance(theorem, Pi):
-        var = theorem.var
-        var_type = theorem.var_type
-
-        print(f"var: {var}, var_type: {print_term(var_type)}")
-
-        context.append((var, var_type))
+        context.append((theorem.var, theorem.var_type))
         theorem = theorem.body
 
-    goal = context.pop()
+    goal = theorem
 
-    print(f"    context: {", ".join([f"{v} : {print_term(t)}" for (v, t) in context])}")
-    print(f"    goal: {print_term(goal[1])}")
+    env: Env = {}
+    arities: dict[str, int] = {}
 
-    theType: str | None = None
+    egraph = EGraph()
 
-    def checkContextTyp(
-        t: Term, theType: str
-    ) -> Literal["theType", "propDef", "funDef", "h"]:
-        if isinstance(t, Sort) and t.level == 1:
-            return "theType"
-        if isinstance(t, Sort) and t.level == 0:
-            return "h"
-        if isinstance(t, Var) and t.name == theType:
-            return "funDef"
+    # HELPERS
+    def isFunction(type: Term, env: Env) -> bool:
+        # check if the type is a function
+        if (
+            isinstance(type, Pi)
+            and isinstance(type.var_type, Var)
+            and env[type.var_type.name] == "type"
+        ):
+            # we have a function application
+            if isinstance(type.body, Var) and env[type.body.name] == "type":
+                return True
+            else:
+                return isFunction(type.body, env)
 
-        print(t)
+        return False
 
-    # TODO: go through the context and build
+    def isPredicate(type: Term, env: Env) -> bool:
+        # check if the type is a function
+        if (
+            isinstance(type, Pi)
+            and isinstance(type.var_type, Var)
+            and env[type.var_type.name] == "type"
+        ):
+            # we have a function application
+            if isinstance(type.body, Sort) and type.body.level == 0:
+                return True
+            else:
+                return isPredicate(type.body, env)
 
-    for c in context:
-        print(checkContextTyp(c, theType))
+        return False
+
+    def compute_arity(t: Term) -> int:
+        count = 0
+        while isinstance(t, Pi):
+            count += 1
+            t = t.body
+        return count
+
+    # ------
+
+    for name, type in context:
+        if isinstance(type, Sort) and type.level == 1 and name:
+            if "type" in env.values():
+                raise RuntimeError("We only support at most one type right now.")
+            print(f"    - Adding new type {name} to env")
+            env[name] = "type"
+        elif isinstance(type, Var) and name and env[type.name] == "type":
+            print(f"    - Adding new constant {name}")
+            env[name] = "constant"
+            _ = egraph.addTerm(Constant(name))
+        elif name and isFunction(type, env):
+            arity = compute_arity(type)
+            arities[name] = arity
+            env[name] = "function"
+            print(f"    - Adding new function {name} with arity {arity}")
+            _ = egraph.addTerm(FunctionSymbol(name, arity))
+        elif name and isPredicate(type, env):
+            arity = compute_arity(type)
+            arities[name] = arity
+            env[name] = "function"
+            print(f"    - Adding new predicate {name} with arity {arity}")
+            _ = egraph.addTerm(PredicateSymbol(name, arity))
+        else:
+            print(f"TODO: Handling {name} with type {print_term(type)}")
